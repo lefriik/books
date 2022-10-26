0ce58f0883f2c62c8661cbe0f1bb4f957951c486
MetaloNN <herrera.a.nicolas@gmail.com>
2022-10-26 19:49:04 -0300

    Create paginas de mantenedores linkeadas

--- ev2/library/urls.py
+++ ev2/library/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
   path('', views.book_list, name='book_list'),
+  path('genero/', views.genre_list, name='genre_list'),
+  path('autor/', views.autor_list, name='autor_list'),
+  path('editorial/', views.publisher_list, name='publisher_list'),
   
 ]
--- ev2/library/views.py
+++ ev2/library/views.py
@@ -8,3 +8,12 @@ from .forms import BookForm
 def book_list(request):
   books = Book.objects.all()
   return render(request, 'library/book_list.html', {'book_list' : books})
+
+def genre_list(request):
+  return render(request, 'library/genre_list.html')
+
+def autor_list(request):
+  return render(request, 'library/autor_list.html')
+
+def publisher_list(request):
+  return render(request, 'library/publisher_list.html') 
