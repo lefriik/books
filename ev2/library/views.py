d8c718a37e9bcd9b58fd5ed389bb3d274f2746d9
lefriik <emerio.isaac.suarez@gmail.com>
2022-10-27 09:29:56 -0300

    Updated: navbar + main content

--- ev2/library/views.py
+++ ev2/library/views.py
@@ -1,7 +1,7 @@
 
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Book, Author
+from .models import Book, Author, Genre, Publisher
 from .forms import BookForm
 
 
@@ -10,11 +10,13 @@ def book_list(request):
   return render(request, 'library/book_list.html', {'book_list' : books})
 
 def genre_list(request):
-  return render(request, 'library/genre_list.html')
+  generos= Genre.objects.all()
+  return render(request, 'library/genre_list.html', {'genre_list' : generos})
 
 def autor_list(request):
   autores = Author.objects.all()
-  return render(request, 'library/autor_list.html', {'autores': autores})
+  return render(request, 'library/autor_list.html', {'author_list': autores})
 
 def publisher_list(request):
-  return render(request, 'library/publisher_list.html') 
+  editoriales = Publisher.objects.all()
+  return render(request, 'library/publisher_list.html', {'publisher_list' : editoriales}) 
