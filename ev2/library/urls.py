27f2cce1f2dac89c94fd1b718360885e39b13bc3
lefriik <emerio.isaac.suarez@gmail.com>
2022-10-28 09:08:09 -0300

    Added: Books Crud + styles

--- ev2/library/urls.py
+++ ev2/library/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
   path('', views.book_list, name='book_list'),
+  path('book/<int:pk>/', views.book_detail, name='book_detail'),
+  path('book/edit/<int:pk>/', views.book_edit, name='book_edit'),
+  path('book/delete/<int:pk>/', views.book_delete, name="book_delete"),
+  path('book/new/', views.book_new, name="book_new" ),
   path('genero/', views.genre_list, name='genre_list'),
   path('autor/', views.autor_list, name='autor_list'),
   path('editorial/', views.publisher_list, name='publisher_list'),
--- ev2/library/views.py
+++ ev2/library/views.py
@@ -5,9 +5,63 @@ from .models import Book, Author, Genre, Publisher
 from .forms import BookForm
 
 
+#------------------------- books ----------------------------------------
+
 def book_list(request):
   books = Book.objects.all()
   return render(request, 'library/book_list.html', {'book_list' : books})
+
+def book_detail(request, pk):
+  book = get_object_or_404(Book, pk=pk)
+  return render(request, 'library/book_detail.html', {'book': book})
+
+
+def book_edit(request, pk):
+
+  book = get_object_or_404(Book, pk=pk)
+
+  if request.method == "POST":
+    form = BookForm(request.POST, instance=book)
+
+    if form.is_valid():
+      book = form.save(commit=False)
+      book.save()
+      return redirect('book_detail', pk=book.pk)
+  else:
+    form = BookForm(instance=book)
+  return render(request, 'library/book_edit.html', {'form': form})
+
+def book_delete(request, pk):
+
+    book = get_object_or_404(Book, pk=pk)
+
+    if request.method=='POST':
+      book.delete()
+      return redirect('book_list')
+      
+    return render(request, 'library/book_detail.html', {'book': book})
+
+
+
+def book_new(request):
+
+  if request.method == "POST":
+    form = BookForm(request.POST)
+
+    if form.is_valid():
+      book = form.save(commit=False)
+      book.save()
+      return redirect('book_detail', pk=book.pk)
+  else:
+    form = BookForm()
+  return render(request, 'library/book_edit.html', {'form': form})
+
+
+
+
+
+
+# ---------------------- Genre -----------------------------------------
 
 def genre_list(request):
   generos= Genre.objects.all()
